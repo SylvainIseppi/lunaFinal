689804a675f9f8042192ca45ef89f84e584c1750
Rudy <rudy.plessis@gmail.com>
2017-09-13 13:27:09 +0200

    Maj TestCategorie

--- src/test/java/com/test/dao/TestCategorieDao.java
+++ src/test/java/com/test/dao/TestCategorieDao.java
@@ -5,18 +5,15 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 
 import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.projet.dao.DaoCategorie;
-import com.projet.dao.DaoCategorieImpl;
 import com.projet.model.Categorie;
 import com.projet.util.Context;
 
-@FixMethodOrder(MethodSorters.JVM)
 public class TestCategorieDao {
 
 	private static DaoCategorie dao;
@@ -28,41 +25,54 @@ public class TestCategorieDao {
 	}
 
 	@Test
-	public void insertCategorie() {
+	@Transactional
+	@Rollback(true)
+	public void insertCategorieTest() {
 		Categorie cat = new Categorie();
 		cat.setLibelle("electromenager");
-
 		dao.insertCategorie(cat);
 		assertEquals((long) cat.getId(), 1);
-
 	}
 
 	@Test
-	public void getAllCat() {
+	@Transactional
+	@Rollback(true)
+	public void getAllCategorieTest() {
+		Categorie cat = new Categorie();
+		dao.insertCategorie(cat);
 		List<Categorie> listcat = dao.getAllCategorie();
 		assertEquals(listcat.size(), 1);
 	}
 
 	@Test
-	public void getOneCat() {
-		Categorie cat = dao.getUneCategorie(1L);
-		assertEquals((long) cat.getId(), 1);
+	@Transactional
+	@Rollback(true)
+	public void getUneCategorieTest() {
+		Categorie cat = new Categorie();
+		dao.insertCategorie(cat);
+		dao.getUneCategorie(1L);
+		assertEquals((long) cat.getId(), 2);
 	}
 
 	@Test
-	public void updateCat() {
-		Categorie cat = dao.getUneCategorie(1L);
-		cat.setLibelle("electromenager2");
+	@Transactional
+	@Rollback(true)
+	public void updateCategorieTest() {
+		Categorie cat = new Categorie();
+		dao.insertCategorie(cat);
+		cat.setLibelle("electromenager");
 		dao.updateCategorie(cat);
-		cat = dao.getUneCategorie(1L);
-		assertEquals(cat.getLibelle(), "electromenager2");
+		cat = dao.getUneCategorie(3L);
+		assertEquals(cat.getLibelle(), "electromenager");
 	}
 
 	@Test
-	@Ignore
-	public void TestDelCat() {
+	@Transactional
+	@Rollback(true)
+	public void deleteCategorieTest() {
+		Categorie cat = new Categorie();
+		dao.insertCategorie(cat);
 		dao.deleteCategorie(dao.getUneCategorie(1L));
 		assertEquals(dao.getUneCategorie(1l), null);
 	}
-
 }
